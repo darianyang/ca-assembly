2d31478b1bb97654bfedb76147327cabd52d23f9
Darian T. Yang <dty7@pitt.edu>
2022-07-27 10:38:40 -0400

    about to run 2 monomer prep on h2p, needed to remake prmtop and inpcrd files first on h2p

--- dimer/md/temp_sed.sh
+++ dimer/md/temp_sed.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # temp_sed.sh
-# replace PDB_TEMP and VER in the std sim scripts dir with target PDB
+# replace 4xfx_sep and v00 in the std sim scripts dir with target PDB
 
 # Parameters
 # ----------
@@ -8,8 +8,8 @@
 # arg 2 = version of the replicate  :   e.g. v00
 
 PDB=$1
-VER=$2
+v00=$2
 
 # apply globally to all files in current directory
-sed -i "s/PDB_TEMP/${PDB}/g" *
-sed -i "s/VER/${VER}/" * 
+sed -i "s/4xfx_sep/${PDB}/g" *
+sed -i "s/v00/${VER}/" * 
